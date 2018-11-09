33dee7c18208a27416d4169eb45539b137731ba0
Kevin Yang <kvnyang503@gmail.com>
2018-11-09 06:48:33 +0000

    created a script in script.js to redirect user to /events/new when they click on the plus button icon. Added comments to index.ejs. Going to add a for loop in there soon to loop through each event that is going to be retreived from the mongo database

--- public/script.js
+++ public/script.js
@@ -21,33 +21,42 @@ var eventimages = document.querySelectorAll(".Events .eventbubble .eventimage");
 
 //Create a for loop with eventbubbles instead
 
-for (var a = 0; a < eventbubbles.length; a++){
+// for (var a = 0; a < eventbubbles.length; a++){
 
 
-    //Find a way to pass the event titles into the addEventListener function below
+//     //Find a way to pass the event titles into the addEventListener function below
 
-    eventbubbles[a].children[0].addEventListener("click", function(){
+//     eventbubbles[a].children[0].addEventListener("click", function(){
         
-        //No other codes work in here because this is technically a function, that makes sense!!!
+//         //No other codes work in here because this is technically a function, that makes sense!!!
 
-        var eventtitle = this.parentElement.children[2].outerText;
+//         var eventtitle = this.parentElement.children[2].outerText;
         
-        window.location.href = "https://personal-web-projects-kevy7.c9users.io/event/" + eventtitle;
+//         window.location.href = "https://personal-web-projects-kevy7.c9users.io/event/" + eventtitle;
         
-        /*
+//         /*
         
-        console.log(this.parentElement.children[2].outerText);
-        This element will return to us the title of the event.
-        How does the code above acheive this?
-            -first, we use "this" to refer to the current eventimage element
-            -.parent will return to us this child's eventubbles element
-            .children[2].outerText will give us the title of the event bubble 
+//         console.log(this.parentElement.children[2].outerText);
+//         This element will return to us the title of the event.
+//         How does the code above acheive this?
+//             -first, we use "this" to refer to the current eventimage element
+//             -.parent will return to us this child's eventubbles element
+//             .children[2].outerText will give us the title of the event bubble 
             
             
-        note: "%20" is used to replace spaces within a url
+//         note: "%20" is used to replace spaces within a url
         
-        */
-    });
+//         */
+//     });
     
     
-}
+// }
+
+
+var plusButton = document.querySelector(".plusbutton");
+
+plusButton.addEventListener("click", function(){
+   
+   window.location.href = "/events/new";
+    
+});
